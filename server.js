e0bad598663db78c1b8110ec48bcae0c5903807c
Muhammad Rainaldy Dharmawan <123210087@student.upnyk.ac.id>
2024-05-07 22:14:22 +0700

    Add package.json & server.js

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const app = express();
+
+const biodata = {
+    nama : "Rainaldy",
+    nim : "12321008",
+    kelas : "IF-E",
+    versi : "app1"
+}
+app.get('/', (req, res) => {
+res.json(biodata);
+});
+// Listen to the App Engine-specified port, or 8080 otherwise
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+console.log(`Server listening on port ${PORT}...`);
+});
